051044cf7a2e2300d01c6eefa4d8093bc1fe9a06
Caedmon Howell <caedmonhowell@gmail.com>
2023-02-14 13:25:15 -0800

    added nav bar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import "./App.css";
+import "./StyleSheets/App.css";
 import React from "react";
 import {Route, Switch, BrowserRouter as Router} from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import SoftwarePage from "./pages/SoftawrePage";
 import CraftsmanPage from "./pages/CraftsmanPage";
+import Navagation from "./Navagation";
 
 function App() {
   return(<div>
+    <Navagation/>
     <Router>
       <Switch>
         <Route exact={true} path="/">
---
+++ src/Navagation.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./StyleSheets/NavBar.css"
+
+function Navagation(){
+    return(<div className="navbar">
+        <a className="col" href="/"><h1>Caedmon Howell</h1></a>
+        <h4 className="col">as a</h4>
+        <ul className="col">
+                <li className="row"><a href ="/software">Software Enginering</a></li>
+                <li className="row"><a href ="/craftsman">Craftman</a></li>
+        </ul>
+    </div>)
+}
+
+export default Navagation;
