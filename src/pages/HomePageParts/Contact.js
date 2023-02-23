38fddfcbc27ea26fe4de8dcc0a3b4049e49bfa89
Caedmon Howell <112980273+CHowell424@users.noreply.github.com>
2023-02-23 14:19:39 -0700

    added to the homepage

--- src/pages/HomePageParts/Contact.js
+++ src/pages/HomePageParts/Contact.js
@@ -3,6 +3,14 @@ import React from "react";
 function Contact(){
     return(<div>
         <h2 className="center-text">Contact</h2>
+        <p className="center-text">You can contact Caedmon in any of the below ways.</p>
+        <div className="c">
+            <a className="contact" href ="https://www.linkedin.com/in/caedmon-howell/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+            <a className="contact" href="https://www.etsy.com/shop/CaveManCraftsByCJ" target="_blank" rel="noopener noreferrer">Etsy</a>
+            <a className="contact" href = "mailto: caedmonhowell@gmail.com" target="_blank" rel="noopener noreferrer">Email</a>
+            <a className="contact" href="https://github.com/CHowell424" target="_blank" rel="noopener noreferrer">Github</a>
+        </div>
+        
     </div>)
 }
 
