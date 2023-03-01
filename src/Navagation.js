3149c417edbb85f7a9d968e27a405829c26b842c
Caedmon Howell <caedmonhowell@gmail.com>
2023-02-28 17:01:17 -0800

    fixed links

--- src/Navagation.js
+++ src/Navagation.js
@@ -1,12 +1,13 @@
 import React from "react";
+import {Link} from "react-router-dom"
 
 function Navagation(){
     return(<div className="container header">
         <h1 className="col center"><a className="col center" href="/">Caedmon Howell</a></h1>
         <h4 className="col center">as a</h4>
         <ul className="col center">
-                <li className="row nav"><a href ="/software">Software Engineer</a></li>
-                <li className="row nav"><a href ="/craftsman">Craftsman</a></li>
+                <li className="row nav"><Link to ="/software">Software Engineer</Link></li>
+                <li className="row nav"><Link to ="/craftsman">Craftsman</Link></li>
         </ul>
     </div>)
 }
--- src/pages/HomePageParts/Career.js
+++ src/pages/HomePageParts/Career.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Code from "../photos/code.png"
+import {Link} from "react-router-dom"
 
 function Coreer(){
     const thinkful = "https://www.thinkful.com//getstarted/all-topics-outcomes/?sf_campaign_id=7015b0000033JLoAAM&utm_campaign=&utm_medium=g&utm_content=&utm_term=GGL_Brand_Ex_GEO_Core&utm_source=google&keyword=thinkful&matchtype=e&adgroupid=125742843081&campaignid=13051883165&creativeid=520765092401&keywordid=303219710301&gclid=Cj0KCQiAutyfBhCMARIsAMgcRJQShfevDhSZ1QNDapSYAcKCFJ-KlsCHXpBbKrmM2XToKUFpXZdYpJIaApFgEALw_wcB";
@@ -8,7 +9,7 @@ function Coreer(){
         <h2 className="center-text">Career</h2>
         <img className="center-image large-image" src={Code} alt="code"/>
         <p>
-        Caedmon is actively looking for a position in <a href ="/software">Software Engineering</a> or Web Development.
+        Caedmon is actively looking for a position in <Link to ="/software">Software Engineering</Link> or Web Development.
         He is excited about the industry because it is a way for him to create and make a difference.
         This has been a 3-year endeavor for Caedmon, starting from when he was in high school taking college classes for software.
 </p>
